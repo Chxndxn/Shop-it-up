49f9336acbd90386ff8f1f15107f8984c820e932
Chxndxn <chandangowda.kr2@gmail.com>
2024-04-07 20:28:24 +0530

    Adding function to get all orders, getting the user id from params

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -2,15 +2,41 @@ const Order = require("../models/order");
 
 exports.getOrdersByUser = async (req, res) => {
   try {
-    const user = req.query.user;
-    if (user) {
-      const ordersList = await Order.find({ user: user });
+    const userId = req.params.id;
+    if (userId) {
+      const ordersList = await Order.find({ user: userId });
       if (ordersList.length > 0) {
         res.status(200).json(ordersList);
       } else {
         res.status(404).json({ error: "No orders found" });
       }
     }
+  } catch (err) {
+    console.error(`Error fetching orders for the user: ${err}`);
+    res.status(500).json({ error: err.message || err });
+  }
+};
+
+exports.getAllOrders = async (req, res) => {
+  try {
+    let query = Order.find({ isActive: true });
+    let totalOrdersQuery = Order.find({ isActive: true });
+    if (req.query._sort && req.query._order) {
+      query = query.sort({ [req.query._sort]: req.query._order });
+    }
+
+    const totalOrders = await totalOrdersQuery.count().exec();
+    console.log({ totalOrders });
+
+    if (req.query._page && req.query._limit) {
+      const pageSize = req.query._limit;
+      const page = req.query._page;
+      query = query.skip(pageSize * (page - 1)).limit(pageSize);
+    }
+
+    const orders = await query.exec();
+    res.set("X-Total-Count", totalOrders);
+    res.status(200).json(orders);
   } catch (err) {
     console.error(`Error fetching orders for the user: ${err}`);
     res.status(500).json({ error: err.message || err });
