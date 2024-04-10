de635a06f169eaa7b074843d1c55289528b78e90
Chxndxn <chandangowda.kr2@gmail.com>
2024-04-10 15:01:30 +0530

    Adding initial cart page

---
+++ client/src/pages/CartPage.jsx
@@ -0,0 +1,14 @@
+import Cart from "../components/cart/Cart";
+import Navbar from "../components/navbar/Navbar";
+
+const CartPage = () => {
+  return (
+    <>
+      <Navbar>
+        <Cart></Cart>
+      </Navbar>
+    </>
+  );
+};
+
+export default CartPage;
