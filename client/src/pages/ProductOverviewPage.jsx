baee711bc496304002cf5d09ae13c8b920cdc8cf
Chxndxn <chandangowda.kr2@gmail.com>
2024-04-10 15:02:54 +0530

    Adding initial product overview page

---
+++ client/src/pages/ProductOverviewPage.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/navbar/Navbar";
+import ProductOverview from "../components/product/ProductOverview";
+
+const ProductOverviewPage = () => {
+  return (
+    <>
+      <Navbar>
+        <ProductOverview></ProductOverview>
+      </Navbar>
+    </>
+  );
+};
+
+export default ProductOverviewPage;
