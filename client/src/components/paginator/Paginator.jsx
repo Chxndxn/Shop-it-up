1a83ae203d694f3ac9c8ae3708d91a21441d50a4
Chxndxn <chandangowda.kr2@gmail.com>
2024-04-17 20:03:35 +0530

    Setting paginator dynamically

--- client/src/components/paginator/Paginator.jsx
+++ client/src/components/paginator/Paginator.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
-const Paginator = () => {
+const Paginator = ({ page, handlePage, pageLimit, totalProducts }) => {
   return (
     <>
       <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
@@ -20,9 +22,15 @@ const Paginator = () => {
         <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
           <div>
             <p className="text-sm text-gray-700">
-              Showing <span className="font-medium">1</span> to{" "}
-              <span className="font-medium">10</span> of{" "}
-              <span className="font-medium">97</span> results
+              Showing{" "}
+              <span className="font-medium">{(page - 1) * pageLimit + 1}</span>{" "}
+              to{" "}
+              <span className="font-medium">
+                {page * pageLimit > totalProducts
+                  ? totalProducts
+                  : page * pageLimit}
+              </span>{" "}
+              of <span className="font-medium">{totalProducts}</span> results
             </p>
           </div>
           <div>
@@ -30,34 +38,45 @@ const Paginator = () => {
               className="isolate inline-flex -space-x-px rounded-md shadow-sm"
               aria-label="Pagination"
             >
-              <a
-                href="#"
-                className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+              <div
+                onClick={() => {
+                  if (page > 1) {
+                    handlePage(page - 1);
+                  }
+                }}
+                className="relative cursor-pointer inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
               >
                 <span className="sr-only">Previous</span>
                 <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-              </a>
+              </div>
               {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
-              <a
-                href="#"
-                aria-current="page"
-                className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                1
-              </a>
-              <a
-                href="#"
-                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                2
-              </a>
-              <a
-                href="#"
-                className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+              {Array.from({ length: Math.ceil(totalProducts / pageLimit) }).map(
+                (el, index) => (
+                  <div
+                    key={index + 1}
+                    onClick={() => handlePage(index + 1)}
+                    aria-current="page"
+                    className={`relative cursor-pointer z-10 inline-flex items-center ${
+                      index + 1 === page
+                        ? "bg-indigo-600 text-white"
+                        : "text-gray-400"
+                    }  px-4 py-2 text-sm font-semibold ring-1 ring-inset ring-gray-300 focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
+                  >
+                    {index + 1}
+                  </div>
+                )
+              )}
+              <div
+                onClick={() => {
+                  if (page * pageLimit < totalProducts) {
+                    handlePage(page + 1);
+                  }
+                }}
+                className="relative cursor-pointer inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
               >
                 <span className="sr-only">Next</span>
                 <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-              </a>
+              </div>
             </nav>
           </div>
         </div>
