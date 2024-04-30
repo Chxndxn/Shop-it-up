8cd262b4eb13924b781d3fcff6e025c3f82326b2
Chxndxn <chandangowda.kr2@gmail.com>
2024-04-30 15:49:55 +0530

    [Bug fix] Change password type to buffer

--- server/models/user.js
+++ server/models/user.js
@@ -6,7 +6,7 @@ const userSchema = new Schema(
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: Buffer, required: true },
     role: { type: String, required: true, default: "user" },
     addresses: { type: [Schema.Types.Mixed] },
     salt: Buffer,
