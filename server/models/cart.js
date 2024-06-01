80e299cac66aa892f83e189ce1a29e603fdc00a7
Chxndxn <chandangowda.kr2@gmail.com>
2024-06-01 21:16:09 +0530

    [Model] Update cart schema

--- server/models/cart.js
+++ server/models/cart.js
@@ -2,12 +2,16 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const cartSchema = new Schema({
-  quantity: { type: Number, required: true },
-  product: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+  products: [
+    { product: { type: Schema.Types.ObjectId, ref: "Product", required: true }, 
+      quantity: { type: Number, required: true }
+    }
+  ],
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  size: { type: Schema.Types.Mixed },
-  color: { type: Schema.Types.Mixed },
-});
+  size: { type: Schema.Types.Mixed, default: undefined },
+  color: { type: Schema.Types.Mixed, default: undefined },
+}, { timestamps: true }
+);
 
 const virtual = cartSchema.virtual("id");
 virtual.get(function () {
