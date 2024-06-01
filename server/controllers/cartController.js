d71ae1dcd7c2e50aeb54057f493616a0fe72a485
Chxndxn <chandangowda.kr2@gmail.com>
2024-06-01 21:24:01 +0530

    [Cart] Update add to cart logic

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -2,74 +2,103 @@ const Cart = require("../models/cart");
 const ObjectId = require("mongoose").Types.ObjectId;
 
 exports.getCartByUser = async (req, res) => {
-  try {
-    const user = req.query.user;
-    if (user) {
-      const cart = await Cart.find({ user: new ObjectId(user) }).populate(
-        "product"
-      );
-      if (cart.length > 0) {
-        res.status(200).json(cart);
-      } else {
-        res.status(404).json({ message: "No items in the cart for the user." });
-      }
-    }
-  } catch (err) {
-    console.error(`Error fetching cart by user: ${err}`);
-    res.status(500).json({ error: err.message || err });
-  }
+	try {
+		const user = req.query.user;
+		if (user) {
+			const cart = await Cart.find({ user: new ObjectId(user) }).populate(
+				"products.product"
+			);
+			if (cart) {
+				res.status(200).json(cart);
+			} else {
+				res.status(404).json({
+					message: "No items in the cart for the user.",
+				});
+			}
+		}
+	} catch (err) {
+		console.error(`Error fetching cart by user: ${err}`);
+		res.status(500).json({ error: err.message || err });
+	}
 };
 
 exports.addToCart = async (req, res) => {
-  try {
-    const cartItems = req.body;
-    if (cartItems.length > 0 || cartItems) {
-      const cart = await Cart.create(cartItems);
-      const result = await Cart.findById(cart._id)
-        .populate("product")
-        .populate("user");
-      res.status(201).json(result);
-    }
-  } catch (err) {
-    console.error(`Error adding to cart: ${err}`);
-    res.status(500).json({ error: err.message || err });
-  }
+	try {
+		const cartItem = req.body;
+		const userId = new ObjectId(cartItem.user);
+		const productId = new ObjectId(cartItem.products.product);
+		const quantity = cartItem.products.quantity;
+		let cart = await Cart.findOne({ user: userId });
+		if (cart) {
+			const productIndex = cart.products.findIndex((item) =>
+				item.product.equals(productId)
+			);
+			if (productIndex > -1) {
+				cart.products[productIndex].quantity += quantity;
+			} else {
+				cart.products.push({
+					product: productId,
+					quantity: quantity,
+				});
+			}
+			cart = await cart.save();
+			cart = await cart.populate("products.product")
+		} else {
+			cart = await Cart.create({
+				user: userId,
+				products: [{ product: productId, quantity: quantity }],
+			});
+			cart = await cart.populate("products.product");
+		}
+		res.status(200).json(cart);
+	} catch (err) {
+		console.error(`Error adding to cart: ${err}`);
+		res.status(500).json({ error: err.message || err });
+	}
 };
 
 exports.updateCart = async (req, res) => {
-  try {
-    const cartId = req.params.id;
-    const cartItems = req.body;
-    if (cartId) {
-      const updatedCart = await Cart.findByIdAndUpdate(cartId, cartItems, {
-        new: true,
-      });
-      if (updatedCart) {
-        const result = await updatedCart.populate("product");
-        res.status(200).json(result);
-      } else {
-        res.status(404).json({ message: "No items in the cart to update." });
-      }
-    }
-  } catch (err) {
-    console.error(`Error updating the cart: ${err}`);
-    res.status(500).json({ error: err.message || err });
-  }
+	try {
+		const cartId = req.params.id;
+		const cartItems = req.body;
+		if (cartId) {
+			const updatedCart = await Cart.findByIdAndUpdate(
+				cartId,
+				cartItems,
+				{
+					new: true,
+				}
+			);
+			if (updatedCart) {
+				const result = await updatedCart.populate("product");
+				res.status(200).json(result);
+			} else {
+				res.status(404).json({
+					message: "No items in the cart to update.",
+				});
+			}
+		}
+	} catch (err) {
+		console.error(`Error updating the cart: ${err}`);
+		res.status(500).json({ error: err.message || err });
+	}
 };
 
 exports.deleteFromCart = async (req, res) => {
-  try {
-    const cartId = req.params.id;
-    if (cartId) {
-      const deletedCart = await Cart.findByIdAndDelete(cartId);
-      if (deletedCart) {
-        res.status(200).json(deletedCart);
-      } else {
-        res.status(404).json({ message: "No items in the cart to delete." });
-      }
-    }
-  } catch (err) {
-    console.error(`Error deleting from cart: ${err}`);
-    res.status(500).json({ error: err.message || err });
-  }
+	try {
+		const cartId = req.params.id;
+		if (cartId) {
+			const deletedCart = await Cart.findByIdAndDelete(cartId);
+			if (deletedCart) {
+				res.status(200).json(deletedCart);
+			} else {
+				res.status(404).json({
+					message: "No items in the cart to delete.",
+				});
+			}
+		}
+	} catch (err) {
+		console.error(`Error deleting from cart: ${err}`);
+		res.status(500).json({ error: err.message || err });
+	}
 };
